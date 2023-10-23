e89ee6b2cf5317d9e48d18d9e390e222cd4a37ff
Anastasia Shipitsyna <anshipitsyna@gmail.com>
2023-10-23 22:10:20 +1200

    добавили новый файл

---
+++ lesson_1_task_1.py
@@ -0,0 +1,3 @@
+my_name = "Anastasia"
+print(my_name)
+
---
+++ lesson_1_task_4.py
@@ -0,0 +1,3 @@
+first_name = input("What is your name?")
+second_name = input("What is your family name?")
+print("Your original name is:", first_name, second_name)                    
