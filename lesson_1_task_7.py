536da702bef3db88542234a069c000dc14c546e5
Anastasia Shipitsyna <anshipitsyna@gmail.com>
2023-10-24 21:12:22 +1200

    исправления 7 и 8 задачь в 1 уроке

--- lesson_1_task_7.py
+++ lesson_1_task_7.py
@@ -1,31 +1,35 @@
 def fun1():
-    print("1")
+    print(1, end='')
 
 def fun2():
-    print("2")
+    print(2, end='')
 
 def fun3():
-    print("3")
+    print(3, end='')
 
 def fun4():
-    print("4")
+    print(4, end='')
 
 def fun5():
-    print("5")
+    print(5, end='')
 
 def fun6():
-    print("6")
+    print(6, end='')
 
 def fun7():
-    print("7")
+    print(7, end='')
 
 def fun8():
-    print("8")
+    print(8, end='')
 
 def fun9():
-    print("9")
+    print(9, end='')
 
 def fun0():
-    print("0")
+    print(0, end='')
 
-fun1(), fun2(), fun3(), fun4(), fun5(), fun6(), fun7(), fun8(), fun9(), fun0()
+fun8(), fun8(), fun0(), fun0(), fun5(), fun5(), fun5(), fun3(), fun5(), fun3(), fun5()
+
+
+
+
--- lesson_1_task_8.py
+++ lesson_1_task_8.py
@@ -1,33 +1,16 @@
 
 #1
-def num(line, count=0):
-    if count < 11:
-        print(line)
-        return num(line + 1, count + 1)
-    else:
-        return line
+def print_number(num):
+    print(num, end='')
 
-result = num(88005553525)
-
-print("_______________________")
-#2
-def num(a, b, c, d, e, f, g, h, m, n, o):
-    print("line  1:", a + b + c)
-    print("line  2:", a + b + c + d)
-    print("line  3:", a + b + c + d + e)
-    print("line  4:", a + b + c + d + e + f)
-    print("line  5:", a + b + c + d + e + f + g)
-    print("line  6:", a + b + c + d + e + f + g + f)
-    print("line  7:", a + b + c + d + e + f + g + f + g)
-    print("line  8:", a + b + c + d + e + f + g + f + g +h)
-    print("line  9:", a + b + c + d + e + f + g + f + g +h + m)
-    print("line 10:", a + b + c + d + e + f + g + f + g +h + m + n)
-    print("line 11:", a + b + c + d + e + f + g + f + g +h + m + n + o)
-
-num(80000000000, 6000000000, 1000000000, 1000000000, 5000000, 500000, 50000, 3000, 500, 30, 5)
-
-
-
-
-
-
+print_number(8)
+print_number(8)
+print_number(0)
+print_number(0)
+print_number(5)
+print_number(5)
+print_number(5)
+print_number(3)
+print_number(3)
+print_number(5)
+print_number(3)
